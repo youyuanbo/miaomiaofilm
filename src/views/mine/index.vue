dc731662a083b953319d5c2970fe0f9561c7bb77
youyuanbo <ug.python.linux@gmail.com>
2019-08-03 16:15:51 +0800

    add create components

---
+++ src/views/mine/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="main">
+        <Header title="我的"/>
+        <div id="content">
+            <Login/>
+        </div>
+        <Footer/>
+    </div>
+</template>
+
+<script>
+    import Header from '@/components/Header';
+    import Footer from '@/components/Footer';
+    import Login from '@/components/login'
+
+    export default {
+        name: "mine",
+        components:{
+            Header,
+            Footer,
+            Login,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
